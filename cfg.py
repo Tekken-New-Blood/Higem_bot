54c31144eb249b9b38468dd7ec728250e3d083fc
eatsports <mousewell1@gmail.com>
2019-03-25 02:09:45 +0300

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -4,8 +4,8 @@
 HOST = 'irc.twitch.tv'
 PORT = 6667
 NICK = 'higem_bot'
-PASS = 'oauth:4kgk9iygvbjol7e5ub707dj1m7otte'
-CHAN = ['dramtheater', 'tokenobduracy', 'asapmarko', 'greenleo', 'forest_exe']
+PASS = ''
+CHAN = []
 RATE = (20/30)  # messages per second
 
 opList = {}
